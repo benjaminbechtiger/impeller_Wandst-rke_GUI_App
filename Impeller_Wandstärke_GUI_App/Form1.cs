28e6600b3964cb75b76976dc35706cf6780dcdde
Benjamin Bechtiger <benjamin.bechtiger@levitronix.com>
2025-09-19 16:44:09 +0200

    Messablauf implementiert

--- Impeller_Wandstärke_GUI_App/Form1.cs
+++ Impeller_Wandstärke_GUI_App/Form1.cs
@@ -1,54 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 using System.Net.Sockets;
 using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
 using Timer = System.Windows.Forms.Timer;
 
 namespace Impeller_Wandstärke_GUI_App
 {
     public partial class Form1 : Form
     {
-        private SerialPort serialPortTMCM1070;
+        // private Variabeln
+        private SerialPort serialPortTMCM1141;
+        private TMCM1141Controller tmcm1141Controller;
         private TcpClient tcpClientKeyence;
         private NetworkStream netStreamKeyence;
         private Thread tcpThreadKeyence;
-        private Timer timerKeyenceRead;
-        private TMCM1070Controller tmcm1070Controller;
+
+        private Timer timerKeyence;
+        private Timer timerTMCM1141;
+        private Timer measurementTimer;
+        private Stopwatch stopwatch;
+
+        private List<long> measurementTimeMs = new List<long>();
+        private List<double> measurementValues = new List<double>();
+
+        private int act_speed_TMCM1141;
+        private double keyence_value;
+        private readonly object keyenceLock = new object();
 
         public Form1()
         {
             InitializeComponent();
-            InitTMCM1070Controller();
+            InitTMCM1141Controller();
             InitKeyenceSensor();
             InitTimerKeyenceSensor();
+            InitTimerTMCM1141();
+            InitMeasurementTimer();
         }
 
-        private void InitTMCM1070Controller()
+        // Hardware Initialisierungen
+
+        private void InitTMCM1141Controller()
         {
             try
             {
-                serialPortTMCM1070 = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+                serialPortTMCM1141 = new SerialPort("COM4", 115200, Parity.None, 8, StopBits.One);
 
-                var task = Task.Run(() => serialPortTMCM1070.Open());
+                var task = Task.Run(() => serialPortTMCM1141.Open());
                 if (!task.Wait(TimeSpan.FromSeconds(5)))
                 {
                     throw new TimeoutException("Es konnte keine UART Verbindung hergestellt werden, Timeout 5000ms");
                 }
-                tmcm1070Controller = new TMCM1070Controller(serialPortTMCM1070);
+                tmcm1141Controller = new TMCM1141Controller(serialPortTMCM1141, LogMessage);
+                tmcm1141Controller.SetPositioningSpeed(10);
 
-                LogMessage("TMCM-1070 Verbindung hergestellt", Color.White);
+                LogMessage("TMCM-1141 Verbindung hergestellt", Color.White);
             }
             catch (Exception ex)
             {
-                LogMessage("TMCM-1070 Fehler: " + ex.Message, Color.Red);
+                LogMessage("TMCM-1141 Fehler: " + ex.Message, Color.Red);
             }
         }
 
@@ -73,7 +88,7 @@ namespace Impeller_Wandstärke_GUI_App
                 tcpClientKeyence.EndConnect(result);
                 netStreamKeyence = tcpClientKeyence.GetStream();
 
-                tcpThreadKeyence = new Thread(ReadTcpLoop);
+                tcpThreadKeyence = new Thread(ReadKeyenceLoop);
                 tcpThreadKeyence.IsBackground = true;
                 tcpThreadKeyence.Start();
 
@@ -86,21 +101,41 @@ namespace Impeller_Wandstärke_GUI_App
             }
         }
 
+
+        // Timer Initialisierungen
+
         private void InitTimerKeyenceSensor()
         {
-            timerKeyenceRead = new Timer();
-            timerKeyenceRead.Interval = 100;
-            timerKeyenceRead.Tick += Timer_Tick_Keyence_Sensor;
-            timerKeyenceRead.Start();
+            timerKeyence = new Timer();
+            timerKeyence.Interval = 50;
+            timerKeyence.Tick += Timer_Tick_Keyence_Sensor;
+            timerKeyence.Start();
         }
 
+        private void InitTimerTMCM1141()
+        {
+            timerTMCM1141 = new Timer();
+            timerTMCM1141.Interval = 50;
+            timerTMCM1141.Tick += Timer_Tick_TMCM1141;
+            timerTMCM1141.Start();
+        }
+
+        private void InitMeasurementTimer()
+        {
+            measurementTimer = new Timer();
+            measurementTimer.Interval = 100;
+            measurementTimer.Tick += MeasurementTimer_Tick;
+            stopwatch = new Stopwatch();
+        }
+
+        // Kontinuerliche Anfragen für den aktuellen Sensormesswert
         private void Timer_Tick_Keyence_Sensor(object sender, EventArgs e)
         {
             if (tcpClientKeyence != null && tcpClientKeyence.Connected)
             {
                 try
                 {
-                    byte[] request = System.Text.Encoding.ASCII.GetBytes("MS,1,1\r");
+                    byte[] request = Encoding.ASCII.GetBytes("MS,1,1\r");
                     netStreamKeyence.Write(request, 0, request.Length);
                 }
                 catch (Exception ex)
@@ -110,6 +145,83 @@ namespace Impeller_Wandstärke_GUI_App
             }
         }
 
+        // Kontinuerliches Auslesen der Schrittmotor-Geschwindigkeit
+        private void Timer_Tick_TMCM1141(object sender, EventArgs e)
+        {
+            tmcm1141Controller.GetActualSpeed(out act_speed_TMCM1141);  
+        }
+
+        // Wenn Messung gestartet wurde, werden Messwerte mit Zeitstempeln gesammelt 
+        // bis die Drehung des Schrittmotors beendet ist (speed == 0)
+        private void MeasurementTimer_Tick(object sender, EventArgs e)
+        {
+            if (act_speed_TMCM1141 > 0)
+            {
+                long elapsedMs = stopwatch.ElapsedMilliseconds;
+                double currentValue;
+
+                buttonStart.Enabled = false;
+
+                lock (keyenceLock)
+                {
+                    currentValue = keyence_value;
+                }
+
+                measurementTimeMs.Add(elapsedMs);
+                measurementValues.Add(currentValue);
+
+                LogMessage($"t={elapsedMs} ms | Wert={currentValue}", Color.LightBlue);
+            }
+            else
+            {
+                int count = measurementValues.Count;
+                int displayCount = Math.Min(10, count);
+
+                buttonStart.Enabled = true;
+
+                LogMessage("Messung beendet", Color.LightBlue);
+                LogMessage("Letzte Messwerte: (..." + string.Join(", ", measurementValues.GetRange(count - displayCount, displayCount)) + ")", Color.LightGreen);
+                LogMessage("Letzte Messzeiten: (..." + string.Join(", ", measurementTimeMs.GetRange(count - displayCount, displayCount)) + ")", Color.LightBlue);
+
+                measurementTimer.Stop();
+                stopwatch.Stop();
+            }
+        }
+
+        // Keyence Thread für das Abholen der Messwerte
+        private void ReadKeyenceLoop()
+        {
+            byte[] buffer = new byte[1024];
+            try
+            {
+                while (netStreamKeyence != null && tcpClientKeyence.Connected)
+                {
+                    int bytes = netStreamKeyence.Read(buffer, 0, buffer.Length);
+                    if (bytes > 0)
+                    {
+                        string msg = Encoding.ASCII.GetString(buffer, 0, bytes).Trim();
+
+                        LogMessage("Keyence Sensor: " + msg, Color.White);
+
+                        labelActValMess.Invoke((Action)(() => labelActValMess.Text = msg));
+
+                        if (double.TryParse(msg, out double value))
+                        {
+                            lock (keyenceLock)
+                            {
+                                keyence_value = value;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage("Keyence Empfangsfehler: " + ex.Message, Color.Red);
+            }
+        }
+
+        // Logfunktion
         private void LogMessage(string message, Color color)
         {
             if (richTextBoxLog.InvokeRequired)
@@ -129,51 +241,32 @@ namespace Impeller_Wandstärke_GUI_App
             }
         }
 
-        private void ReadTcpLoop()
-        {
-            byte[] buffer = new byte[1024];
-            try
-            {
-                while (netStreamKeyence != null && tcpClientKeyence.Connected)
-                {
-                    int bytes = netStreamKeyence.Read(buffer, 0, buffer.Length);
-                    if (bytes > 0)
-                    {
-                        string msg = Encoding.ASCII.GetString(buffer, 0, bytes);
-                        LogMessage("Keyence Sensor: " + msg, Color.White);
-                        labelActValMess.Invoke((Action)(() => labelActValMess.Text = msg));
-                    }
-                }         
-            }
-            catch (Exception ex)
-            {
-                LogMessage("Keyence Empfangsfehler: " + ex.Message, Color.Red);
-            }
-        }
-
+        // Messung Starten gedrückt
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            if (tmcm1070Controller != null)
+            if (tmcm1141Controller != null)
             {
                 try
                 {
-                    if (tmcm1070Controller.Write(5, 1, 0, 0))                      //Position Achse 0 auf 0 microsteps setzen
+                    if (tmcm1141Controller.ResetPosition())                         //Position Achse Zurücksetzen
                     {
-                        bool success = tmcm1070Controller.Write(4, 0, 0, 12800);   //Achse 0 auf 12800 microsteps bewegen (1 Rot)
+                        bool success = tmcm1141Controller.MovePositionAbs(1);       //1 Umdrehung
 
                         if (success)
                         {
+                            stopwatch.Restart();
+                            measurementTimer.Start();
                             LogMessage("Rotation gestartet", Color.White);
                         }
                         else
                         {
-                            LogMessage("Kommunikation mit TMCM-1070 nicht möglich", Color.Red);
+                            LogMessage("Kommunikation mit TMCM-1141 nicht möglich", Color.Red);
                         }
                     }
                 }
                 catch (Exception ex)
                 {
-                    LogMessage("TMCM-1070 Ausnahme: " + ex.Message, Color.Red);
+                    LogMessage("TMCM-1141 Ausnahme: " + ex.Message, Color.Red);
                 }
             }
         }
---
+++ Impeller_Wandstärke_GUI_App/TMCM1141Controller.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.IO.Ports;
+using System.Drawing;
+
+namespace Impeller_Wandstärke_GUI_App
+{
+    class TMCM1141Controller
+    {
+        private readonly SerialPort _serial;
+        private readonly byte _slaveAddress;
+        private readonly Action<string, Color> _logger;
+        private bool serial_was_open = false;
+
+        private byte axis = 2;
+
+        // Konstruktor
+        public TMCM1141Controller(SerialPort serial, Action<string, Color> logger = null, byte slaveAddress = 2)
+        {
+            _serial = serial ?? throw new ArgumentNullException(nameof(serial));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _slaveAddress = slaveAddress;
+        }
+
+        // Datenlogger
+        private void Log(string message, Color color)
+        {
+            _logger?.Invoke(message, color);
+        }
+
+        /// <summary>
+        /// Sendet einen TMCL Befehl an den TMCM-3110 Controller.
+        /// Stellt sicher, dass die serielle Verbindung offen ist und versucht ggf. neu zu verbinden.
+        /// </summary>
+        private bool SendTMCLCommand(byte command, byte type, uint value, out int replyValue, int timeoutMs = 50)
+        {
+            replyValue = 0;
+
+            try
+            {
+                if (_serial == null)
+                    Log("Serial port object is null.", Color.White);
+
+                if (!_serial.IsOpen)
+                {
+                    try
+                    {
+                        _serial.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        if (serial_was_open) { Log($"[WARN] Unable to open serial port: {ex.Message}", Color.Yellow); }
+                        serial_was_open = false;
+                    }
+                }
+
+                // Frame bauen
+                byte[] frame = new byte[9];
+                frame[0] = _slaveAddress;
+                frame[1] = command;
+                frame[2] = type;
+                frame[3] = axis;
+                frame[4] = (byte)((value >> 24) & 0xFF);
+                frame[5] = (byte)((value >> 16) & 0xFF);
+                frame[6] = (byte)((value >> 8) & 0xFF);
+                frame[7] = (byte)(value & 0xFF);
+
+                // Checksumme
+                byte checksum = 0;
+                for (int i = 0; i < 8; i++)
+                    checksum += frame[i];
+                frame[8] = checksum;
+
+                // Senden
+                _serial.Write(frame, 0, frame.Length);
+                _serial.BaseStream.Flush();
+
+                // Antwort abwarten
+                DateTime start = DateTime.Now;
+                while (_serial.BytesToRead < 9)
+                {
+                    if ((DateTime.Now - start).TotalMilliseconds > timeoutMs)
+                        return false;
+                    System.Threading.Thread.Sleep(1);
+                }
+
+                // Antwort lesen
+                byte[] response = new byte[9];
+                int read = _serial.Read(response, 0, 9);
+                if (read < 9) return false;
+
+                // Prüfsumme prüfen
+                byte sum = 0;
+                for (int i = 0; i < 8; i++)
+                    sum += response[i];
+                if (sum != response[8]) return false;
+
+                // Status prüfen
+                if (response[2] != 100) return false;
+
+                // 32-bit Wert auslesen
+                replyValue = (response[4] << 24) |
+                                (response[5] << 16) |
+                                (response[6] << 8) |
+                                response[7];
+
+                if (!serial_was_open) { Log("Serial port reconnected", Color.White); }
+                serial_was_open = true;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (serial_was_open) { Log($"[ERROR] Serial communication failed: {ex.Message}", Color.Red); }
+
+                try { if (_serial.IsOpen) _serial.Close(); } catch { }
+            }
+
+            return false;
+        }
+
+        // Abstrahierte TMCL Write Funktion
+        private bool Write(byte command, byte type, uint value)
+        {
+            return SendTMCLCommand(command, type, value, out _);
+        }
+
+        // Abstrahierte TMCL Read Funktion
+        private bool Read(byte command, byte type, out int value)
+        {
+            return SendTMCLCommand(command, type, 0, out value);
+        }
+
+        // Referenzfahrt starten
+        public bool StartReferenceSearch()
+        {
+            return Write(13, 0, 0);
+        }
+
+        // Auf eine Absolute Position in Umdrehungen bewegen (+/-)
+        public bool MovePositionAbs(double revolutions)
+        {
+            int usteps_per_revolution = 12800;
+
+            double position_usteps_double = Math.Round(revolutions * usteps_per_revolution, 3);
+            int position_usteps = (int)Math.Round(position_usteps_double, MidpointRounding.AwayFromZero);
+            uint position_uint = unchecked((uint)position_usteps);
+
+            return Write(4, 0, position_uint);
+        }
+
+        // Achsenposition in Umdrehungen auslesen
+        public bool GetActualPosition(out double position_rev)
+        {
+            position_rev = 0;
+
+            bool read_status = Read(6, 1, out int position_usteps);
+
+            int usteps_per_revolution = 12800;
+
+            position_rev = Math.Round((double)position_usteps / usteps_per_revolution, 3);
+            return read_status;
+        }
+
+        // 
+        public bool GetActualSpeed(out int speed_upm)
+        {
+            bool read_status = Read(6, 3, out int speed);
+
+            double speed_pps = speed * 30.5177 + 0.1105;
+            speed_upm = (int)Math.Round(speed_pps * 60 / 12800, MidpointRounding.AwayFromZero);
+
+            return read_status;
+        }
+
+        // Achse anhalten
+        public bool MotorStop()
+        {
+            if (axis > 2) return false;
+            return Write(3, 0, 0);
+        }
+
+        // Achse kontinuierlich nach rechts drehen, speed in U/min
+        public bool RotateRight(int speed_upm)
+        {
+            double speed_pps = (double)speed_upm * 12800 / 60;
+            int speed = (int)Math.Round((speed_pps - 0.1105) / 30.5177, MidpointRounding.AwayFromZero);
+
+            return Write(1, 0, unchecked((uint)speed));
+        }
+
+        // Achse kontinuierlich nach links drehen, speed in U/min
+        public bool RotateLeft(int speed_upm)
+        {
+            double speed_pps = (double)speed_upm * 12800 / 60;
+            int speed = (int)Math.Round((speed_pps - 0.1105) / 30.5177, MidpointRounding.AwayFromZero);
+
+            return Write(2, 0, unchecked((uint)speed));
+        }
+
+        // Achsenposition auf 0 setzen
+        public bool ResetPosition()
+        {
+            return Write(5, 1, 0);
+        }
+
+        public bool SetPositioningSpeed(int speed_upm)
+        {
+            double speed_pps = (double)speed_upm * 12800 / 60;
+            int speed = (int)Math.Round((speed_pps - 0.1105) / 30.5177, MidpointRounding.AwayFromZero);
+
+            return Write(5, 4, unchecked((uint)speed));
+        }
+    }
+}
